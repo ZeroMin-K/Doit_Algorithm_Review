c30eb554d981b7df50861e791c05c0245519d6e9
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-03-31 23:06:08 +0900

    Create median3.py

---
+++ chapter01/median3.py
@@ -0,0 +1,24 @@
+# 세 정수를 입력받아 중앙값 구하기 1
+
+def med3(a, b, c):
+    # a, b, c 중앙값을 구하여 반환
+    if a >= b:
+        if b >= c:
+            return b
+        elif a <= c:
+            return a
+        else:
+            return c
+    elif a > c :
+        return a
+    elif b > c:
+        return c
+    else:
+         return b
+
+print('세 정수의 중앙값 계산')
+a = int(input('정수 a의 값 입력: '))
+b = int(input('정수 b의 값 입력: '))
+c = int(input('정수 c의 값 입력: '))
+
+print(f'중앙값 = {med3(a, b, c)}')
