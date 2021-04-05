635e86843693a8af14acaaf8c58dca088c572a34
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-04-05 22:34:50 +0900

    Create alternative1.py

---
+++ chapter01/alternative1.py
@@ -0,0 +1,12 @@
+# +와 -를 번갈아 출력. 
+
+print('+와 -를 번갈어 출력.')
+n = int(input('몇 개를 출력?: '))
+
+for i in range(n):
+    if i % 2:
+        print('-', end='')
+    else:
+        print('+', end='')
+
+print()
