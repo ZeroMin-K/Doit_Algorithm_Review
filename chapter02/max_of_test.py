e138bb9c5ba500afb4d3e882675ea6ae4365b79a
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-04-23 19:38:00 +0900

    Create max_of_test.py

---
+++ chapter02/max_of_test.py
@@ -0,0 +1,10 @@
+# 각 배열 원소의 최댓값을 구해서 출력
+
+from max import max_of
+t = (4, 7, 5.6, 2, 3.14, 1)
+s = 'string'
+a = ['DTS', 'AAC', 'FLAC']
+
+print(f'{t}의 최댓값 {max_of(t)}')
+print(f'{s}의 최댓값 {max_of(s)}')
+print(f'{a}의 최댓값 {max_of(a)}')
