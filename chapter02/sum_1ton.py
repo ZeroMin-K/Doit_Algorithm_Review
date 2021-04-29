d86877bb5624031481703af588e69f85c353a0d2
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-04-29 23:37:13 +0900

    Create sum_1ton.py

---
+++ chapter02/sum_1ton.py
@@ -0,0 +1,12 @@
+# 1부터 n까지 정수의 합 구하기
+
+def sum_1ton(n):
+    # 1부터 n까지 정수의 합
+    s = 0
+    while n > 0:
+        s += n
+        n -= 1
+    return s
+
+x = int(input('x의 값 입력: '))
+print(f'1부터 {x}까지 정수의 합: {sum_1ton(x)}')
