a7438f974e202b8bb64c2097959fbe4112f9d7dd
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-06-24 18:57:56 +0900

    Update fixed_queue.py

--- chapter04/fixed_queue.py
+++ chapter04/fixed_queue.py
@@ -31,3 +31,64 @@ class FixedQueue:
     def is_full(self) -> bool:
         # 큐가 가득차있는지 판단
         return self.no >= self.capacity 
+    
+    def enque(self, x: Any) -> None:
+        # 데이터 x 인큐
+        if self.is_full():
+            raise FixedQueue.Full   # 큐가 가득차 있는 경우 예외처리 발생
+        self.que[self.rear] = x
+        self.rear += 1
+        self.no += 1
+        if self.rear == self.capacity:
+            self.rear = 0 
+
+    def deque(self) -> Any:
+        # 데이터를 디큐
+        if self.is_empty():
+            raise FixedQueue.Empty  # 큐가 비어있는 경우 예외 처리 발생
+        x = self.que[self.front]
+        self.front += 1
+        self.no -= 1
+        if self.front == self.capacity:
+            self.front = 0
+        return x
+
+    def peek(self) -> Any:
+        # 큐에서 데이터를 피크
+        if self.is_empty():
+            raise FixedQueue.Empty      # 큐가 비어있는 경우 예외처리 발생
+        return self.que[self.front]
+
+    def find(self, value: Any) -> Any:
+        # 큐에서 value를 찾아 인덱스를 반환
+        for i in range(self.no):
+            idx = (i + self.front) % self.capacity
+            if self.que[idx] == value:      # 검색 성공
+                return idx
+        return -1       #  검색 실패 
+        
+    def count(self, value: Any) -> bool:
+        # 큐에 있는 value의 개수 반환
+        c = 0
+        for i in range(self.no):    # 큐 데이터를 선형검색
+            idx = (i+ self.front) % self.capacity
+            if self.que[idx] == value:      # 검생 성공
+                c += 1                      # 들어 있음
+        return c
+
+    def __contains__(self, value: Any) -> bool:
+        # 큐에 value가 있는지 판단
+        return self.count(value)
+
+    def clear(self) -> None:
+        # 큐의 모든 데이터 비움
+        self.no = self.front = self.rear = 0
+
+    def dump(self) -> None:
+        # 모든 데이터를 맨 앞부터 맨 끝순으로 출력
+        if self.is_empty():
+            print('큐가 비어있음')
+        else:
+            for i in range(self.no):
+                print(self.que[(i+self.front) % self.capacity], end='')
+            print()
