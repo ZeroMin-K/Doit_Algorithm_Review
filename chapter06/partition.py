75c10ef94a03dbd138afab939241444275bf14ab
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-07-06 13:39:53 +0900

    Create partition.py

---
+++ chapter06/partition.py
@@ -0,0 +1,42 @@
+# 배열을 두 그룹으로 나누기 
+
+from typing import MutableSequence
+
+def partition(a: MutableSequence) -> None:
+    # 배열을 나누어 출력
+    n = len(a)
+    pl = 0  # 왼쪽 커서
+    pr = n - 1  # 오른 쪽 커서 
+    x = a[n // 2]   # 피벗 
+
+    while pl <= pr:
+        while a[pl] < x: 
+            pl += 1
+        while a[pr] > x:
+            pr -= 1
+        if pl <= pr:
+            a[pl], a[pr] = a[pr], a[pl]
+            pl += 1
+            pr -= 1
+
+    print(f'피벗 {x}')
+
+    print('피벗 이하 그룹')
+    print(*a[0: pl])
+
+    if pl > pr + 1:
+        print('피벗과 일치하는 그룹')
+        print(*a[pr + 1 : pl])
+
+    print('피벗 이상인 그룹')
+    print(*a[pr+1:n])
+
+if __name__ == '__main__':
+    print('배열을 나눔')
+    num = int(input('원소 수 입력: '))
+    x = [None] * num
+
+    for i in range(num):
+        x[i] = int(input(f'x[{i}]: '))
+
+    partition(x)
